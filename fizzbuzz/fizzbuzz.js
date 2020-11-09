451db7f539d0487e96d1cf7d062b329829813996
Meng Fu <Ellie@Ellies-MacBook-Air.local>
2020-11-08 20:20:37 -0600

    Responsive Resume

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+console.log('HELLO FIZZBUZZ')
+const i = 0
+
+while (i < 1000) {
+
+  if (i % 3 === 0) {
+    console.log('fizz', i)
+  }
+  i++
+} 
+
+  if (i % 15 == 0) console.log("Fizz Buzz");
+  else if (i % 3 === 0) console.log("Fizz");
+  else if (i % 5 === 0) console.log("Buzz");while (i < 1000) {
+
+  if (i % 15 == 0) console.log("Fizz Buzz");
+  else if (i % 3 === 0) console.log("Fizz");
+  else if (i % 5 === 0) console.log("Buzz");
+}   i++
+
---
+++ resume-interactive.js
@@ -0,0 +1,24 @@
+//add fade in when click to name on resume 
+
+//display call me  your phone number until it clicked on 
+
+
+// hover on picture to make it fade 
+
+const callMeFunction=() => {
+document.getElementById("callMe").innerHTML ="Call This Number!" 
+}
+
+const fadeOut =() =>{
+  console.log("hello")
+  document.getElementById("picture").style.opacity ="0"
+}
+
+const fadeIn =() =>{
+  document.getElementById("picture").style.opacity ="1"
+}
+
+const textColor=()=> {
+  document.getElementById("title").style.color="white"
+}
+
